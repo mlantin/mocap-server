a9c166f901ea54afd0f91fb6a8cd7164b8a041a8
Maria Lantin <maria.lantin@gmail.com>
2016-04-22 18:09:10 -0700

    Initial commit with a stub server.

---
+++ src/Unix/CMakeFiles/mocap-server.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mlantin/Documents/Research/VR/mocap-server/src/mocap-server.cpp" "/Users/mlantin/Documents/Research/VR/mocap-server/src/Unix/CMakeFiles/mocap-server.dir/mocap-server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/Users/mlantin/Documents/Vicon_DataStream_SDK_1.3.0.57137h_MAC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Unix/CMakeFiles/mocap-server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mocap-server.dir/mocap-server.cpp.o"
+  "../bin/mocap-server.pdb"
+  "../bin/mocap-server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mocap-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

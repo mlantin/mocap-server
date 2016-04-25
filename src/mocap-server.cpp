63ad2531b2b64768970fb005fc0844aefd32f5d9
mlantin <maria.lantin@gmail.com>
2016-04-25 01:14:49 -0700

    basically pulled the bits that I think I'll need from the sample Vicon server. Next need to integrate the websocket. I found a nice simple implementation call easywsclient that I think I'll use.

--- src/mocap-server.cpp
+++ src/mocap-server.cpp
@@ -256,6 +256,256 @@ int main( int argc, char* argv[] )
   #endif
     }
     std::cout << std::endl;
-  }
 
+    // Enable some different data types
+    MyClient.EnableSegmentData();
+    MyClient.EnableMarkerData();
+    MyClient.EnableUnlabeledMarkerData();
+    MyClient.EnableDeviceData();
+
+    std::cout << "Segment Data Enabled: "          << Adapt( MyClient.IsSegmentDataEnabled().Enabled )         << std::endl;
+    std::cout << "Marker Data Enabled: "           << Adapt( MyClient.IsMarkerDataEnabled().Enabled )          << std::endl;
+    std::cout << "Unlabeled Marker Data Enabled: " << Adapt( MyClient.IsUnlabeledMarkerDataEnabled().Enabled ) << std::endl;
+    std::cout << "Device Data Enabled: "           << Adapt( MyClient.IsDeviceDataEnabled().Enabled )          << std::endl;
+
+    // Set the streaming mode
+    //MyClient.SetStreamMode( ViconDataStreamSDK::CPP::StreamMode::ClientPull );
+    // MyClient.SetStreamMode( ViconDataStreamSDK::CPP::StreamMode::ClientPullPreFetch );
+    MyClient.SetStreamMode( ViconDataStreamSDK::CPP::StreamMode::ServerPush );
+
+    // Set the global up axis
+    MyClient.SetAxisMapping( Direction::Forward, 
+                             Direction::Left, 
+                             Direction::Up ); // Z-up
+    // MyClient.SetGlobalUpAxis( Direction::Forward, 
+    //                           Direction::Up, 
+    //                           Direction::Right ); // Y-up
+
+    Output_GetAxisMapping _Output_GetAxisMapping = MyClient.GetAxisMapping();
+    std::cout << "Axis Mapping: X-" << Adapt( _Output_GetAxisMapping.XAxis ) 
+                           << " Y-" << Adapt( _Output_GetAxisMapping.YAxis ) 
+                           << " Z-" << Adapt( _Output_GetAxisMapping.ZAxis ) << std::endl;
+
+    // Discover the version number
+    Output_GetVersion _Output_GetVersion = MyClient.GetVersion();
+    std::cout << "Version: " << _Output_GetVersion.Major << "." 
+                             << _Output_GetVersion.Minor << "." 
+                             << _Output_GetVersion.Point << std::endl;
+
+    if( EnableMultiCast )
+    {
+      assert( MyClient.IsConnected().Connected );
+      MyClient.StartTransmittingMulticast( HostName, MulticastAddress );
+    }
+
+    size_t FrameRateWindow = 1000; // frames
+    size_t Counter = 0;
+    clock_t LastTime = clock();
+    // Loop until a key is pressed
+  #ifdef WIN32
+    while( !Hit() )
+  #else
+    while( true)
+  #endif
+    {
+      // Get a frame
+      output_stream << "Waiting for new frame...";
+      while( MyClient.GetFrame().Result != Result::Success )
+      {
+        // Sleep a little so that we don't lumber the CPU with a busy poll
+        #ifdef WIN32
+          Sleep( 200 );
+        #else
+          sleep(1);
+        #endif
+
+        output_stream << ".";
+      }
+      output_stream << std::endl;
+      if(++Counter == FrameRateWindow)
+      {
+        clock_t Now = clock();
+        double FrameRate = (double)(FrameRateWindow * CLOCKS_PER_SEC) / (double)(Now - LastTime);
+        if(!LogFile.empty())
+        {
+          time_t rawtime;
+          struct tm * timeinfo;
+          time ( &rawtime );
+          timeinfo = localtime ( &rawtime );
+
+          ofs << "Frame rate = " << FrameRate << " at " <<  asctime (timeinfo)<< std::endl;
+        }
+
+        LastTime = Now;
+        Counter = 0;
+      }
+
+      // Get the frame number
+      Output_GetFrameNumber _Output_GetFrameNumber = MyClient.GetFrameNumber();
+      output_stream << "Frame Number: " << _Output_GetFrameNumber.FrameNumber << std::endl;
+
+      Output_GetFrameRate Rate = MyClient.GetFrameRate();
+      std::cout << "Frame rate: "           << Rate.FrameRateHz          << std::endl;
+      // Get the timecode
+      Output_GetTimecode _Output_GetTimecode  = MyClient.GetTimecode();
+
+      output_stream << "Timecode: "
+                << _Output_GetTimecode.Hours               << "h "
+                << _Output_GetTimecode.Minutes             << "m " 
+                << _Output_GetTimecode.Seconds             << "s "
+                << _Output_GetTimecode.Frames              << "f "
+                << _Output_GetTimecode.SubFrame            << "sf "
+                << Adapt( _Output_GetTimecode.FieldFlag ) << " " 
+                << _Output_GetTimecode.Standard            << " " 
+                << _Output_GetTimecode.SubFramesPerFrame   << " " 
+                << _Output_GetTimecode.UserBits            << std::endl << std::endl;
+
+      // Get the latency
+      output_stream << "Latency: " << MyClient.GetLatencyTotal().Total << "s" << std::endl;
+      
+      for( unsigned int LatencySampleIndex = 0 ; LatencySampleIndex < MyClient.GetLatencySampleCount().Count ; ++LatencySampleIndex )
+      {
+        std::string SampleName  = MyClient.GetLatencySampleName( LatencySampleIndex ).Name;
+        double      SampleValue = MyClient.GetLatencySampleValue( SampleName ).Value;
+
+        output_stream << "  " << SampleName << " " << SampleValue << "s" << std::endl;
+      }
+      output_stream << std::endl;
+
+      // Count the number of subjects
+      unsigned int SubjectCount = MyClient.GetSubjectCount().SubjectCount;
+      output_stream << "Subjects (" << SubjectCount << "):" << std::endl;
+      for( unsigned int SubjectIndex = 0 ; SubjectIndex < SubjectCount ; ++SubjectIndex )
+      {
+        output_stream << "  Subject #" << SubjectIndex << std::endl;
+
+        // Get the subject name
+        std::string SubjectName = MyClient.GetSubjectName( SubjectIndex ).SubjectName;
+        output_stream << "    Name: " << SubjectName << std::endl;
+
+        // Get the root segment
+        std::string RootSegment = MyClient.GetSubjectRootSegmentName( SubjectName ).SegmentName;
+        output_stream << "    Root Segment: " << RootSegment << std::endl;
+
+        // Count the number of segments
+        unsigned int SegmentCount = MyClient.GetSegmentCount( SubjectName ).SegmentCount;
+        output_stream << "    Segments (" << SegmentCount << "):" << std::endl;
+        for( unsigned int SegmentIndex = 0 ; SegmentIndex < SegmentCount ; ++SegmentIndex )
+        {
+          output_stream << "      Segment #" << SegmentIndex << std::endl;
+
+          // Get the segment name
+          std::string SegmentName = MyClient.GetSegmentName( SubjectName, SegmentIndex ).SegmentName;
+          output_stream << "        Name: " << SegmentName << std::endl;
+
+          // Get the segment parent
+          std::string SegmentParentName = MyClient.GetSegmentParentName( SubjectName, SegmentName ).SegmentName;
+          output_stream << "        Parent: " << SegmentParentName << std::endl;
+
+          // Get the segment's children
+          unsigned int ChildCount = MyClient.GetSegmentChildCount( SubjectName, SegmentName ).SegmentCount;
+          output_stream << "     Children (" << ChildCount << "):" << std::endl;
+          for( unsigned int ChildIndex = 0 ; ChildIndex < ChildCount ; ++ChildIndex )
+          {
+            std::string ChildName = MyClient.GetSegmentChildName( SubjectName, SegmentName, ChildIndex ).SegmentName;
+            output_stream << "       " << ChildName << std::endl;
+          }
+
+          // Get the static segment translation
+          Output_GetSegmentStaticTranslation _Output_GetSegmentStaticTranslation = 
+            MyClient.GetSegmentStaticTranslation( SubjectName, SegmentName );
+          output_stream << "        Static Translation: (" << _Output_GetSegmentStaticTranslation.Translation[ 0 ]  << ", " 
+                                                       << _Output_GetSegmentStaticTranslation.Translation[ 1 ]  << ", " 
+                                                       << _Output_GetSegmentStaticTranslation.Translation[ 2 ]  << ")" << std::endl;
+
+          // Get the static segment rotation in quaternion co-ordinates
+          Output_GetSegmentStaticRotationQuaternion _Output_GetSegmentStaticRotationQuaternion = 
+            MyClient.GetSegmentStaticRotationQuaternion( SubjectName, SegmentName );
+          output_stream << "        Static Rotation Quaternion: (" << _Output_GetSegmentStaticRotationQuaternion.Rotation[ 0 ]     << ", " 
+                                                               << _Output_GetSegmentStaticRotationQuaternion.Rotation[ 1 ]     << ", " 
+                                                               << _Output_GetSegmentStaticRotationQuaternion.Rotation[ 2 ]     << ", " 
+                                                               << _Output_GetSegmentStaticRotationQuaternion.Rotation[ 3 ]     << ")" << std::endl;
+
+          // Get the global segment translation
+          Output_GetSegmentGlobalTranslation _Output_GetSegmentGlobalTranslation = 
+            MyClient.GetSegmentGlobalTranslation( SubjectName, SegmentName );
+          output_stream << "        Global Translation: (" << _Output_GetSegmentGlobalTranslation.Translation[ 0 ]  << ", " 
+                                                       << _Output_GetSegmentGlobalTranslation.Translation[ 1 ]  << ", " 
+                                                       << _Output_GetSegmentGlobalTranslation.Translation[ 2 ]  << ") " 
+                                                       << Adapt( _Output_GetSegmentGlobalTranslation.Occluded ) << std::endl;
+
+          
+          // Get the global segment rotation in quaternion co-ordinates
+          Output_GetSegmentGlobalRotationQuaternion _Output_GetSegmentGlobalRotationQuaternion = 
+            MyClient.GetSegmentGlobalRotationQuaternion( SubjectName, SegmentName );
+          output_stream << "        Global Rotation Quaternion: (" << _Output_GetSegmentGlobalRotationQuaternion.Rotation[ 0 ]     << ", " 
+                                                               << _Output_GetSegmentGlobalRotationQuaternion.Rotation[ 1 ]     << ", " 
+                                                               << _Output_GetSegmentGlobalRotationQuaternion.Rotation[ 2 ]     << ", " 
+                                                               << _Output_GetSegmentGlobalRotationQuaternion.Rotation[ 3 ]     << ") " 
+                                                               << Adapt( _Output_GetSegmentGlobalRotationQuaternion.Occluded ) << std::endl;
+
+          // Get the local segment translation
+          Output_GetSegmentLocalTranslation _Output_GetSegmentLocalTranslation = 
+            MyClient.GetSegmentLocalTranslation( SubjectName, SegmentName );
+          output_stream << "        Local Translation: (" << _Output_GetSegmentLocalTranslation.Translation[ 0 ]  << ", " 
+                                                      << _Output_GetSegmentLocalTranslation.Translation[ 1 ]  << ", " 
+                                                      << _Output_GetSegmentLocalTranslation.Translation[ 2 ]  << ") " 
+                                                      << Adapt( _Output_GetSegmentLocalTranslation.Occluded ) << std::endl;
+
+
+          // Get the local segment rotation in quaternion co-ordinates
+          Output_GetSegmentLocalRotationQuaternion _Output_GetSegmentLocalRotationQuaternion = 
+            MyClient.GetSegmentLocalRotationQuaternion( SubjectName, SegmentName );
+          output_stream << "        Local Rotation Quaternion: (" << _Output_GetSegmentLocalRotationQuaternion.Rotation[ 0 ]     << ", " 
+                                                              << _Output_GetSegmentLocalRotationQuaternion.Rotation[ 1 ]     << ", " 
+                                                              << _Output_GetSegmentLocalRotationQuaternion.Rotation[ 2 ]     << ", " 
+                                                              << _Output_GetSegmentLocalRotationQuaternion.Rotation[ 3 ]     << ") " 
+                                                              << Adapt( _Output_GetSegmentLocalRotationQuaternion.Occluded ) << std::endl;
+
+        }
+
+        // Count the number of markers
+        unsigned int MarkerCount = MyClient.GetMarkerCount( SubjectName ).MarkerCount;
+        output_stream << "    Markers (" << MarkerCount << "):" << std::endl;
+        for( unsigned int MarkerIndex = 0 ; MarkerIndex < MarkerCount ; ++MarkerIndex )
+        {
+          // Get the marker name
+          std::string MarkerName = MyClient.GetMarkerName( SubjectName, MarkerIndex ).MarkerName;
+
+          // Get the marker parent
+          std::string MarkerParentName = MyClient.GetMarkerParentName( SubjectName, MarkerName ).SegmentName;
+
+          // Get the global marker translation
+          Output_GetMarkerGlobalTranslation _Output_GetMarkerGlobalTranslation =
+            MyClient.GetMarkerGlobalTranslation( SubjectName, MarkerName );
+
+          output_stream << "      Marker #" << MarkerIndex            << ": "
+                                        << MarkerName             << " ("
+                                        << _Output_GetMarkerGlobalTranslation.Translation[ 0 ]  << ", "
+                                        << _Output_GetMarkerGlobalTranslation.Translation[ 1 ]  << ", "
+                                        << _Output_GetMarkerGlobalTranslation.Translation[ 2 ]  << ") "
+                                        << Adapt( _Output_GetMarkerGlobalTranslation.Occluded ) << std::endl;
+        }
+      }
+
+      
+      
+    if( EnableMultiCast )
+    {
+      MyClient.StopTransmittingMulticast();
+    }
+    MyClient.DisableSegmentData();
+    MyClient.DisableMarkerData();
+    MyClient.DisableUnlabeledMarkerData();
+    MyClient.DisableDeviceData();
+
+    // Disconnect and dispose
+    int t = clock();
+    std::cout << " Disconnecting..." << std::endl;
+    MyClient.Disconnect();
+    int dt = clock() - t;
+    double secs = (double) (dt)/(double)CLOCKS_PER_SEC;
+    std::cout << " Disconnect time = " << secs << " secs" << std::endl;
+
+  }
 }
